9b9ab53476d0793f0cb00438cc87890131312dfd
Ian Cottam <ian.cottam@bbc.co.uk>
2018-11-28 19:02:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+
+
+var element = document.createElement('h1');
+element.id = 'victory';
+element.innerText = 'Ian is the champion!';
+document.body.appendChild(element)
+
+var main = document.getElementById('main')
+main.remove();
